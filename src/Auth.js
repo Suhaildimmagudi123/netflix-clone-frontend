a44b66b5cd0c6d1c4073085c0ab78a887fe8faf8
Suhail Dimmagudi <dimmagudisuhail502@gmail.com>
2026-03-04 16:44:40 +0530

    Fix auth API URL

--- src/Auth.js
+++ src/Auth.js
@@ -3,7 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import './Auth.css';
 
-const API_BASE = 'http://localhost:8000/api';
+const API_BASE = 'https://netflix-clone-078z.onrender.com/api';
 
 function Auth() {
   const [isLogin, setIsLogin] = useState(true);
